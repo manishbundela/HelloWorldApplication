99a335c6bf02da1f7e779b681bd44d16e1604e86
manishbundela <47292463+manishbundela@users.noreply.github.com>
2025-06-21 22:09:53 +0530

    Update Tester1.java
    02 add

--- src/main/java/com/nareshit/helloworld/Tester1.java
+++ src/main/java/com/nareshit/helloworld/Tester1.java
@@ -2,7 +2,7 @@ package com.nareshit.helloworld;
 
 public class Tester1 {
   public static void main(String[] args) {
-	  int c=0;
+	  int c=02;
 	System.out.println("I am tester. here for tester!"+c);
 }
 }
