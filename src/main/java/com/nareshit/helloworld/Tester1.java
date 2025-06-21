3e1dff9509d811ff347d5d4a74881e419dd10c42
manishbundela <manishsunrays@gmail.com>
2025-06-21 17:53:07 +0530

    X is added

--- src/main/java/com/nareshit/helloworld/Tester1.java
+++ src/main/java/com/nareshit/helloworld/Tester1.java
@@ -2,6 +2,7 @@ package com.nareshit.helloworld;
 
 public class Tester1 {
   public static void main(String[] args) {
-	System.out.println("I am tester. here for tester!");
+	  int c=0;
+	System.out.println("I am tester. here for tester!"+c);
 }
 }
